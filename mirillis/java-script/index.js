5f44c996fff5fc8ab63e4056c1f26489a3886904
staspostoyev <mr.postoyev@gmail.com>
2020-04-05 15:32:21 +0200

    add sliders show

--- mirillis/java-script/index.js
+++ mirillis/java-script/index.js
@@ -9,4 +9,18 @@ menu.addEventListener('click', () => {
         }
 })
 
+let myIndex = 0;
+carousel();
+
+function carousel() {
+  let i;
+  const slideShow = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slideShow.length; i++) {
+    slideShow[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > slideShow.length) {myIndex = 1}    
+  slideShow[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 4000);
+}
 
